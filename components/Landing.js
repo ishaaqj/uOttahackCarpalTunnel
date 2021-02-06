f297ccdf2d0e536a314381a4e8e654bd8962cfd9
ishaaqj <info@ishaaq.net>
2021-02-06 13:31:37 -0500

    Bottom tab

--- components/Landing.js
+++ components/Landing.js
@@ -1,14 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { Text, View, Button } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import LandingScreen from "./components/Landing";
+import TrainingScreen from "./components/Training";
+
+const Tab = createBottomTabNavigator();
 
 export default function Landing({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Button
-        title="Training"
-        onPress={() => navigation.navigate("Training")}
-      />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Landing" component={LandingScreen} />
+      <Tab.Screen name="Training" component={TrainingScreen} />
+    </Tab.Navigator>
   );
 }
