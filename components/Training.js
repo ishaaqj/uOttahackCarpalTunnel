919f86f415e593d64092c321ab3190dcaefdf144
ishaaqj <info@ishaaq.net>
2021-02-07 00:41:13 -0500

    timer works

--- components/Training.js
+++ components/Training.js
@@ -1,26 +1,24 @@
-import { NavigationContainer } from "@react-navigation/native";
 import React, { Fragment } from "react";
 import { SafeAreaView, StyleSheet, Text, View, Button } from "react-native";
-import { Video } from "expo-av";
-import Carousel, { Pagination } from "react-native-snap-carousel";
-import CarouselCardItem, { SLIDER_WIDTH, ITEM_WIDTH } from "./CarouselCardItem";
 import CarouselCards from "./CarouselCards";
-import Stopwatch from "./stopwatch/stopwatch";
+import StopwatchContainer from "./stopwatch/stopwatch";
 
 export default function Training({ navigation }) {
   const [index, setIndex] = React.useState(0);
   return (
-    <SafeAreaView style={styles.container}>
-      <CarouselCards />
-    </SafeAreaView>
+    <Fragment>
+      <SafeAreaView style={styles.container}>
+        <CarouselCards />
+        <StopwatchContainer />
+      </SafeAreaView>
+    </Fragment>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    height: 600,
     padding: 50,
   },
 });
--- components/stopwatch/stopwatch.js
+++ components/stopwatch/stopwatch.js
@@ -1,12 +1,5 @@
 import React, { Component } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Button,
-  Pressable,
-} from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 let padToTwo = (number) => (number <= 9 ? `0${number}` : number);
 
@@ -18,6 +11,7 @@ export class StopwatchContainer extends Component {
       min: 0,
       sec: 0,
       msec: 0,
+      start: false,
     };
 
     this.lapArr = [];
@@ -41,20 +35,20 @@ export class StopwatchContainer extends Component {
   handleStart = () => {
     if (this.state.start) {
       this.interval = setInterval(() => {
-        if (this.state.msec !== 99) {
+        if (this.state.msec !== 0) {
           this.setState({
-            msec: this.state.msec + 1,
+            msec: this.state.msec - 1,
           });
-        } else if (this.state.sec !== 59) {
+        } else if (this.state.sec !== 0) {
           this.setState({
-            msec: 0,
-            sec: ++this.state.sec,
+            msec: 59,
+            sec: --this.state.sec,
           });
         } else {
           this.setState({
             msec: 0,
             sec: 0,
-            min: ++this.state.min,
+            min: 0,
           });
         }
       }, 1);
@@ -66,7 +60,7 @@ export class StopwatchContainer extends Component {
   handleReset = () => {
     this.setState({
       min: 0,
-      sec: 0,
+      sec: 10,
       msec: 0,
 
       start: false,
